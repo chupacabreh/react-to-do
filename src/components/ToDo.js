3d983c10fef44b50fd243d41d0f05d479359e2ec
Matt Freitas <mattfreitas415@gmail.com>
2017-11-21 21:12:09 -0800

    created component

---
+++ src/components/ToDo.js
@@ -0,0 +1,15 @@
+// 1. import React and React's Component Calss
+import React, { Component } from 'react';
+
+
+// 2. Define a class that extends Component
+class ToDo extends Component {
+  // 3. Define a render() method on your class.
+  render() {
+    return (
+      <li>A todo will go here</li>
+    );
+  }
+}
+// 4. export the class
+export default ToDo;
